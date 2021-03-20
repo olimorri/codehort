508d29c3b76882f4c2ac871f2221e7d41ca98b37
Colin Bellamy <colin.j.bellamy@gmail.com>
2021-03-20 16:57:55 +0000

    feat(lesson/navbar): add navbar component to lesson

--- client/src/containers/Lesson/Lesson.tsx
+++ client/src/containers/Lesson/Lesson.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import NavBar from '../../components/NavBar/NavBar';
 import InstructionList from '../../components/InstructionList/InstructionList';
 import CodeEditor from '../../components/CodeEditor/CodeEditor';
 import TaskList from '../../components/TaskList/TaskList';
@@ -7,11 +8,11 @@ import TaskList from '../../components/TaskList/TaskList';
 export default function Landing(): JSX.Element {
   return (
     <div>
+      <NavBar />
       <h1>Lesson 1</h1>
       <InstructionList />
       <CodeEditor />
       <TaskList />
-      <Link to="/dashboard">Dashboard</Link>
     </div>
   );
 }
