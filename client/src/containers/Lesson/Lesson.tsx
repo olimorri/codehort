9478e8ffa730ba0829a807481d196d98aaf0b5e3
Colin Bellamy <colin.j.bellamy@gmail.com>
2021-03-20 14:51:36 +0000

    feat(lesson): add instructions component with example to lesson

--- client/src/containers/Lesson/Lesson.tsx
+++ client/src/containers/Lesson/Lesson.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Instructions from '../../components/Instructions/Instructions';
 import CodeEditor from '../../components/CodeEditor/CodeEditor';
 import TaskList from '../../components/TaskList/TaskList';
 
 export default function Landing(): JSX.Element {
   return (
     <div>
-      <h1>I am a Lesson</h1>
+      <h1>Lesson 1</h1>
+      <Instructions />
       <CodeEditor />
       <TaskList />
       <Link to="/dashboard">Dashboard</Link>
