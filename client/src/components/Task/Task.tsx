de1c77f040c33c2ceab4d5c809dff29b17241b1a
Colin Bellamy <colin.j.bellamy@gmail.com>
2021-03-20 14:44:51 +0000

    feat(lesson.tsx): add task and tasklist components

---
+++ client/src/components/Task/Task.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Task(): JSX.Element {
+  return (
+    <div>
+      <h1>I am a Task</h1>
+    </div>
+  );
+}
--- client/src/containers/Lesson/Lesson.tsx
+++ client/src/containers/Lesson/Lesson.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CodeEditor from '../../components/CodeEditor/CodeEditor';
+import TaskList from '../../components/TaskList/TaskList';
 
 export default function Landing(): JSX.Element {
   return (
     <div>
       <h1>I am a Lesson</h1>
+      <CodeEditor />
+      <TaskList />
       <Link to="/dashboard">Dashboard</Link>
     </div>
   );
