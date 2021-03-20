34fc66ded9678b24554a593261fb0be9b2dda411
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-03-20 11:27:10 +0000

    feat: working text editor and some other small changes to linting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import TextEditor from './components/text_editor';
 
 function App(): any {
   return (
     <div className="App">
-      <h1> Hello world!!!!!!!!!!!</h1>
+      <TextEditor />
     </div>
   );
 }
