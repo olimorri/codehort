59f44b3ad4430fd683695aa3ecf4b3542420bf88
Colin Bellamy <colin.j.bellamy@gmail.com>
2021-03-20 15:20:18 +0000

    build(index.tsx): install and import redux packages

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import logger from 'redux-logger';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './containers/App/App';
 
