b9db73789cbd9aadc9d435ea4449d178c4c4d1c0
Colin Bellamy <colin.j.bellamy@gmail.com>
2021-03-21 12:54:25 +0000

    feat(error.tsx): change error page content

--- client/src/containers/Error/Error.tsx
+++ client/src/containers/Error/Error.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 export default function Error(): JSX.Element {
   return (
     <div>
-      <h1>Oops! Page not found!</h1>
+      <h1>4😮4</h1>
+      <h2>Oops!</h2>
+      <h2>We can't seem to find the page you're looking for.</h2>
     </div>
   );
 }
