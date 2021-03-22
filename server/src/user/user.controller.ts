7f69167d653a9c31f9f109c164860d19956454bb
AJ <joachim.liedtke@t-online.de>
2021-03-22 11:05:49 +0100

    feat(user.service.ts, user.controller.ts): add basic login

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,17 +1,22 @@
 import { Controller, Post, Body, HttpCode } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private userService: UserService) {}
+
   @Post('/register')
-  register(@Body() createUserDto: CreateUserDto): string {
-    return createUserDto.username;
+  register(@Body() newUser: CreateUserDto): CreateUserDto {
+    this.userService.createUser(newUser.username, newUser.password);
+    console.log(this.userService.users);
+    return newUser;
   }
 
   @Post('login')
   @HttpCode(200)
-  login(): string {
-    return 'You have logged in. Hello!';
+  login(@Body() user: CreateUserDto): string {
+    return this.userService.loginUser(user.username, user.password);
   }
 
   @Post('logout')
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  // TEMPORARY User db
+  users: CreateUserDto[] = [
+    {
+      username: 'JohnDoe',
+      password: '12345',
+    },
+    {
+      username: 'JaneDoe',
+      password: '12345',
+    },
+  ];
+
+  createUser(username: string, password: string) {
+    const newUser = { username, password };
+    this.users.push(newUser);
+  }
+
+  loginUser(username: string, password: string) {
+    const user = this.finUser(username);
+    return user.password === password ? `Welcome back, ${username}` : 'could not log in';
+  }
+
+  // Helper functions
+  finUser(username: string) {
+    const user = this.users.find((user) => user.username === username);
+    if (user) return user;
+    else throw new NotFoundException();
+  }
+}
