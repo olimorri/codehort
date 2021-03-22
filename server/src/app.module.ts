4ff9e8ce2e608bb966a68d45ef6bd47b6d278e2e
AJ <joachim.liedtke@t-online.de>
2021-03-22 10:06:19 +0100

    feat(user.module.ts): add user module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
