12d048b8f93b9ae519f16da6c55425eae487e8f5
AJ <joachim.liedtke@t-online.de>
2021-03-22 18:49:32 +0100

    feat(task): set up task module, controller, service and dto

---
+++ server/src/task/dto/task.dto.ts
@@ -0,0 +1,9 @@
+export class TaskDto {
+  name: string;
+  step: number;
+  explanation: string;
+  test: any; // TODO: change data type
+  hints: any; // TODO: change data type
+  answer: any; // TODO: change data type
+  summary: any; // TODO: change data type
+}
--- server/src/task/task.module.ts
+++ server/src/task/task.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TaskController } from './task.controller';
+import { TaskService } from './task.service';
 
-@Module({})
+@Module({
+  controllers: [TaskController],
+  providers: [TaskService],
+})
 export class TaskModule {}
