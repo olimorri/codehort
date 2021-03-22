221df7846c55469f3b93d1122ecab3a19fadadfc
Colin Bellamy <colin.j.bellamy@gmail.com>
2021-03-22 14:01:39 +0000

    refactor(index.sass): change path names on components

--- client/src/containers/Dashboard/Dashboard.tsx
+++ client/src/containers/Dashboard/Dashboard.tsx
@@ -10,11 +10,11 @@ export default function Landing(): JSX.Element {
       <div className="content">
         <div className="left">
           <h2 className="subheader">Your Lessons</h2>
+          <Link to="/lesson:id">Get started</Link>
         </div>
         <div className="right">
           <h2 className="subheader">Your Rewards</h2>
           <h2 className="subheader">Ready for more?</h2>
-          <Link to="/lesson:id">Get started</Link>
         </div>
       </div>
     </div>
