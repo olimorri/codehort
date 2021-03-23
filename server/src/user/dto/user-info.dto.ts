3adf4da52c9bdeac0a294d27f121290a80d62306
AJ <joachim.liedtke@t-online.de>
2021-03-23 10:24:02 +0100

    fix(user-info.dto): adjust dto to match schema

--- server/src/user/dto/user-info.dto.ts
+++ server/src/user/dto/user-info.dto.ts
@@ -1,8 +1,10 @@
+import { Lesson } from 'src/lesson/lesson.schema';
+
 export class UserInfoDto {
   id: string;
   username: string;
-  email: string;
   password: string;
-  lessons?: string[];
-  rewardsUnlocked?: string;
+  email: string;
+
+  lessons?: Lesson[];
 }
