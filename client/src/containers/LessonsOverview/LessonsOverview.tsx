6030c8b33d7187cb42276bc346902dfa167f8611
Colin Bellamy <colin.j.bellamy@gmail.com>
2021-03-23 14:53:22 +0000

    style(lessonsoverview): add container structure and heading styling

--- client/src/containers/LessonsOverview/LessonsOverview.tsx
+++ client/src/containers/LessonsOverview/LessonsOverview.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 export default function LessonsOverview(): JSX.Element {
   return (
-    <div>
-      <h1>What will you learn next?</h1>
+    <div className="lessons-overview">
+      <div className="header">
+        <h1>What will you learn next?</h1>
+      </div>
+      <div className="content"></div>
     </div>
   );
 }
