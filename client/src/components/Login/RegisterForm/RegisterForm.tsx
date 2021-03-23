344fb3ca59503596060a60e5bb94972af68b060b
Colin Bellamy <colin.j.bellamy@gmail.com>
2021-03-23 16:44:31 +0000

    refactor(registerform): add inputs for registerform and remove autocomplete

--- client/src/components/Login/RegisterForm/RegisterForm.tsx
+++ client/src/components/Login/RegisterForm/RegisterForm.tsx
@@ -7,19 +7,33 @@ export default function RegisterForm(): JSX.Element {
   return (
     <FormTemplate>
       <form className="register-form">
+        <label htmlFor="username">username</label>
+        <div className="form-input">
+          <IconContext.Provider value={{ size: '2em', className: 'carrot' }}>
+            <RiArrowRightSLine />
+          </IconContext.Provider>
+          <input type="text" name="username" autoComplete="off" />
+        </div>
         <label htmlFor="email">email address</label>
         <div className="form-input">
           <IconContext.Provider value={{ size: '2em', className: 'carrot' }}>
             <RiArrowRightSLine />
           </IconContext.Provider>
-          <input type="email" name="email"></input>
+          <input type="email" name="email" autoComplete="off" />
         </div>
         <label htmlFor="password">password</label>
         <div className="form-input">
           <IconContext.Provider value={{ size: '2em', className: 'carrot' }}>
             <RiArrowRightSLine />
           </IconContext.Provider>
-          <input type="password" name="password" />
+          <input type="password" name="password" autoComplete="off" />
+        </div>
+        <label htmlFor="reenter-password">re-enter password</label>
+        <div className="form-input">
+          <IconContext.Provider value={{ size: '2em', className: 'carrot' }}>
+            <RiArrowRightSLine />
+          </IconContext.Provider>
+          <input type="password" name="reenter-password" autoComplete="off" />
         </div>
         <button type="submit" className="button">
           Enter
