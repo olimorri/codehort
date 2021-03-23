d0d6dedc16a58bc99601838ffef5de76880a3780
AJ <joachim.liedtke@t-online.de>
2021-03-23 19:07:11 +0100

    feat(app): add api route to create new lessons in one go

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('create-lesson')
+  async createLesson(@Body() newLesson): Promise<string> {
+    return await this.appService.createLesson(newLesson);
   }
 }
