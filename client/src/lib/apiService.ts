116d563418d1af3ad499f83d69e01877dcda93b3
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-03-24 20:41:31 +0000

    feat: userLogout added - this needs work though

--- client/src/lib/apiService.ts
+++ client/src/lib/apiService.ts
@@ -23,10 +23,17 @@ export function getLesson(): Promise<ILesson> {
   return fetchRequest('/lesson/1');
 }
 
+//TODO: TEST
 export function userLogin(username: string, password: string): Promise<IUser> {
   return fetchRequest(`/user/login`, { body: { username, password } });
 }
 
+//TODO: This need to be finalised, I think we need to send additional data - username - as well as handling this on FE
+export function userLogout(): Promise<void> {
+  return fetchRequest('/logout');
+}
+
+//TODO: TEST
 export function getUser(username: string): Promise<IUser> {
   return fetchRequest(`/user/profile/${username}`);
 }
