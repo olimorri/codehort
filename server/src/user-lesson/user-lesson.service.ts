6a31eebcc56f328fb450372acba5cecdd858489a
AJ <joachim.liedtke@t-online.de>
2021-03-24 17:23:50 +0100

    refactor(user-lesson.service.ts): code cleanup

--- server/src/user-lesson/user-lesson.service.ts
+++ server/src/user-lesson/user-lesson.service.ts
@@ -9,7 +9,6 @@ export class UserLessonService {
     newUserLesson.stepCompleted = userLessonDto.stepCompleted;
     newUserLesson.userId = userLessonDto.userId;
     newUserLesson.lessonId = userLessonDto.lessonId;
-
     try {
       return await newUserLesson.save();
     } catch (error) {
@@ -18,33 +17,14 @@ export class UserLessonService {
   }
 
   async updateUserLesson(updatedUserLesson: UserLessonDto): Promise<UserLessonDto> {
-    const updatedLesson = new UserLesson();
-    updatedLesson.userId = updatedUserLesson.userId;
-    updatedLesson.lessonId = updatedUserLesson.lessonId;
-    updatedLesson.stepCompleted = updatedUserLesson.stepCompleted;
-    console.log(updatedLesson);
     try {
       return UserLesson.findOne({
-        where: { userId: updatedLesson.userId, lessonId: updatedLesson.lessonId },
+        where: { userId: updatedUserLesson.userId, lessonId: updatedUserLesson.lessonId },
       }).then((updatedLesson) => {
-        console.log(updatedLesson.stepCompleted);
         return updatedLesson.update({
           stepCompleted: updatedUserLesson.stepCompleted,
-          // userId: updatedLesson.userId,
-          // lessonId: updatedLesson.lessonId,
         });
       });
-
-      // return await updatedLesson.update(
-      //   {
-      //     stepCompleted: updatedLesson.stepCompleted,
-      //     userId: updatedLesson.userId,
-      //     lessonId: updatedLesson.lessonId,
-      //   },
-      //   {
-      //     where: { userId: updatedLesson.userId, lessonId: updatedLesson.lessonId },
-      //   }
-      // );
     } catch (error) {
       console.log(error);
     }
