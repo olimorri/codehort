40458f3ace206b9c10d135563424943fd44dd4ce
AJ <joachim.liedtke@t-online.de>
2021-03-24 22:43:22 +0100

    feat(lesson.controller.ts): sort tasks, hints and summaries before returning lesson

--- server/src/lesson/lesson.controller.ts
+++ server/src/lesson/lesson.controller.ts
@@ -16,6 +16,19 @@ export class LessonController {
   async getLesson(@Param('id') id: number): Promise<LessonDto> {
     const newLesson = await this.lessonService.fetchLesson(id);
     newLesson.task.sort((a, b) => (a.step >= b.step ? 1 : -1));
+
+    newLesson.task.forEach((task) => {
+      if (task.hints?.length > 1) {
+        task.hints.sort((a, b) => (a.title >= b.title ? 1 : -1));
+      }
+    });
+
+    newLesson.task.forEach((task) => {
+      if (task.summaries?.length > 1) {
+        task.summaries.sort((a, b) => (a.id >= b.id ? 1 : -1));
+      }
+    });
+
     return newLesson;
   }
 }
