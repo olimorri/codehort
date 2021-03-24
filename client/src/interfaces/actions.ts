16b5acea2e4244931856f847567236c0e987ac6f
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-03-24 21:47:36 +0000

    feat(actions): add userLesson action

--- client/src/interfaces/actions.ts
+++ client/src/interfaces/actions.ts
@@ -1,7 +1,8 @@
-import { ILesson, IUser } from '.';
+import { ILesson, IUser, IUserLesson } from '.';
 
 export const SET_LESSON = 'SET_LESSON';
 export const SET_USER = 'SET_USER';
+export const SET_USER_LESSON = 'SET_USER_LESSON';
 
 export interface ILessonAction {
   type: typeof SET_LESSON;
@@ -13,4 +14,9 @@ export interface IUserAction {
   payload: IUser;
 }
 
-export type AppActions = ILessonAction | IUserAction;
+export interface IUserLessonAction {
+  type: typeof SET_USER_LESSON;
+  payload: IUserLesson;
+}
+
+export type AppActions = ILessonAction | IUserAction | IUserLessonAction;
