34f599bef3b1ded035d9b57a2df0a452f2a77b6b
Colin Bellamy <colin.j.bellamy@gmail.com>
2021-03-24 15:20:20 +0000

    feat(apiservice): add getLesson apiservice

---
+++ client/src/lib/apiService.ts
@@ -0,0 +1,23 @@
+import { ILesson } from '../interfaces/lesson';
+
+const baseUrl: string = 'http://localhost:3001';
+
+function fetchRequest(path: string, options?): Promise<any> {
+  return fetch(baseUrl + path)
+    .then((res: Response) => {
+      if (res.status <= 400) {
+        Promise.reject();
+      } else if (res.status === 204) {
+        return res;
+      } else {
+        return res.json();
+      }
+    })
+    .catch((error) => {
+      console.error(`Error fetching ${path}`, error);
+    });
+}
+
+export default function getLesson(): Promise<ILesson> {
+  return fetchRequest('/lesson/1');
+}
