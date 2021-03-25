61478195693909502d66e89141d51ddbf148e121
AJ <joachim.liedtke@t-online.de>
2021-03-25 19:56:06 +0100

    feat(navbar.tsx): implement logout

--- client/src/components/App/NavBar/NavBar.tsx
+++ client/src/components/App/NavBar/NavBar.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setUser } from '../../../actions/user';
 
 export default function NavBar(): JSX.Element {
+  const dispatch = useDispatch();
+
+  // TODO: proper logout when implementing authentication
+  const handleClick = () => {
+    dispatch(
+      setUser({
+        id: '',
+        username: '',
+        password: '',
+        email: '',
+      })
+    );
+  };
+
   return (
     <div className="nav-bar">
       <Link to="/dashboard" className="link big-link">
@@ -14,7 +30,7 @@ export default function NavBar(): JSX.Element {
         <Link to="/dashboard" className="link small-link">
           profile
         </Link>
-        <Link to="/" className="link small-link">
+        <Link to="/" className="link small-link" onClick={handleClick}>
           logout
         </Link>
       </ul>
