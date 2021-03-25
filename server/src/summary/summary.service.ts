8b3d5175a39e92dc08eeccfe7774fba41268697e
AJ <joachim.liedtke@t-online.de>
2021-03-25 11:09:39 +0100

    fix(summary module): fix error handling

--- server/src/summary/summary.service.ts
+++ server/src/summary/summary.service.ts
@@ -1,20 +1,20 @@
-import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { SummaryDto } from './dto/summary.dto';
 import { Summary } from './summary.schema';
 
 @Injectable()
 export class SummaryService {
   async createSummaries(newSummaries: SummaryDto[]): Promise<void> {
-    newSummaries.forEach((summary) => {
-      const newSummary = new Summary();
-      Object.assign(newSummary, summary); // assign keys from Summary to newSummary
-      try {
+    try {
+      newSummaries.forEach((summary) => {
+        const newSummary = new Summary();
+        Object.assign(newSummary, summary); // assign keys from Summary to newSummary
         newSummary.save();
-      } catch (error) {
-        console.log(error);
-        throw new InternalServerErrorException('Summaries could not be saved');
-      }
-    });
+      });
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException('An internal server error occured');
+    }
   }
 
   async getSummary(summaryId: number) {
@@ -22,7 +22,7 @@ export class SummaryService {
       return await Summary.findOne({ where: { id: summaryId } });
     } catch (error) {
       console.log(error);
-      throw new NotFoundException('Summaries could not be found');
+      throw new InternalServerErrorException('An internal server error occured');
     }
   }
 }
