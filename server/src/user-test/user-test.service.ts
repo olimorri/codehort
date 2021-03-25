8774e07e255600214e43227126c6665f44c30e01
AJ <joachim.liedtke@t-online.de>
2021-03-25 11:11:28 +0100

    fix(user-test module): fix error handling

--- server/src/user-test/user-test.service.ts
+++ server/src/user-test/user-test.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { UserTestDto } from './dto/user-test.dto';
 import { UserTest } from './userTest.schema';
 
@@ -6,19 +6,32 @@ import { UserTest } from './userTest.schema';
 export class UserTestService {
   async createUserTests(newUserTests: UserTestDto[]): Promise<void> {
     newUserTests.forEach((test) => {
-      const newTest = new UserTest();
-      Object.assign(newTest, test); // add keys from the test object to the newTest instance
       try {
+        const newTest = new UserTest();
+        Object.assign(newTest, test); // add keys from the test object to the newTest instance
         newTest.save();
       } catch (error) {
         console.log(error);
+        throw new InternalServerErrorException('An internal server error occured');
       }
     });
   }
+
   async getSingleTest(testId: number) {
-    return await UserTest.findOne({ where: { id: testId } });
+    try {
+      return await UserTest.findOne({ where: { id: testId } });
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException('An internal server error occured');
+    }
   }
+
   async getTests(taskId: number) {
-    return await UserTest.findAll({ where: { taskId: taskId } });
+    try {
+      return await UserTest.findAll({ where: { taskId: taskId } });
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException('An internal server error occured');
+    }
   }
 }
