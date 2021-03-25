db8ae989df2e82cd8933fe0bb7bbe3ecab9dbd04
Colin Bellamy <colin.j.bellamy@gmail.com>
2021-03-25 18:40:52 +0000

    fix(instructions): uncomment code

--- client/src/components/Lesson/Instructions/Instructions.tsx
+++ client/src/components/Lesson/Instructions/Instructions.tsx
@@ -6,7 +6,7 @@ export default function Instructions(): JSX.Element {
   const tasks = useSelector((state: AppState) => state.lesson.lesson.task);
   const userLesson = useSelector((state: AppState) => state.userLesson.userLesson); //does this need to be a specific lesson
   console.log(userLesson);
-  // const userStep = userLesson.stepsCompleted;
+  const userStep = userLesson.stepsCompleted;
 
-  return <div className="instructions">{/* <p>{tasks && tasks[userStep].explanation}</p> */}</div>;
+  return <div className="instructions">{<p>{tasks && tasks[userStep].explanation}</p>}</div>;
 }
