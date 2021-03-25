7b18c3d0016571520a0fec7f48ac4ce2c98a40b3
AJ <joachim.liedtke@t-online.de>
2021-03-25 12:50:51 +0100

    feat(login): get working on login

--- client/src/components/Login/LoginForm/LoginForm.tsx
+++ client/src/components/Login/LoginForm/LoginForm.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import FormTemplate from '../FormTemplate/FormTemplate';
 import { IconContext } from 'react-icons';
 import { RiArrowRightSLine } from 'react-icons/ri';
+import { userLogin } from '../../../lib/apiService';
 
 export default function LoginForm(): JSX.Element {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = (event: any) => {
+    if (event.target.id === 'username') setUsername(event.target.value);
+    if (event.target.id === 'password') setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    userLogin(username, password);
+  };
+
   return (
     <FormTemplate>
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleSubmit}>
         <label htmlFor="username">username</label>
         <div className="form-input">
           <IconContext.Provider value={{ size: '2em', className: 'carrot' }}>
             <RiArrowRightSLine />
           </IconContext.Provider>
-          <input type="text" name="username" autoComplete="off" />
+          <input
+            type="text"
+            id="username"
+            name="username"
+            autoComplete="off"
+            value={username}
+            onChange={handleChange}
+          />
         </div>
         <label htmlFor="password">password</label>
         <div className="form-input">
           <IconContext.Provider value={{ size: '2em', className: 'carrot' }}>
             <RiArrowRightSLine />
           </IconContext.Provider>
-          <input type="password" name="password" autoComplete="off" />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            autoComplete="off"
+            value={password}
+            onChange={handleChange}
+          />
         </div>
         <button type="submit" className="button">
           Enter
