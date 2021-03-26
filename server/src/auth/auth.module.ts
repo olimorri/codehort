a8194166b7f45d24d782cf782e49bc9ecac0e0dc
AJ <joachim.liedtke@t-online.de>
2021-03-26 18:25:11 +0100

    feat(auth.module.ts): add import for UserModule

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from 'src/user/user.module';
 import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthService]
+  imports: [UserModule],
+  providers: [AuthService],
 })
 export class AuthModule {}
