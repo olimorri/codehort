a04ecb9fb78a826703b8bab2130d6a35f9acdf73
AJ <joachim.liedtke@t-online.de>
2021-03-26 18:56:23 +0100

    feat(app.controller.ts): add auth/login route

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,11 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller, Body, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { User } from './user/user.schema';
+import { LocalAuthGuard } from './auth/local-auth.guards';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
   @Post('create-lesson')
   async createLesson(@Body() newLesson): Promise<string> {
     return await this.appService.createLesson(newLesson);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req): Promise<User> {
+    return req.user;
   }
 }
