21c565db2cf4805e001b68d1e25eb28762c82f9d
AJ <joachim.liedtke@t-online.de>
2021-03-26 23:10:16 +0100

    feat(auth.service.ts): add jwt login route

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { User } from 'src/user/user.schema';
 import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private jwtService: JwtService) {}
 
   async validateUser(username: string, pass: string): Promise<User> {
     const user = await this.userService.getUserInfo(username);
@@ -15,5 +16,10 @@ export class AuthService {
       return thisUser;
     }
     return null;
+  }
+
+  async login(user: User) {
+    const payload = { username: user.username, sub: user.id };
+    return { access_token: this.jwtService.sign(payload) };
   }
 }
