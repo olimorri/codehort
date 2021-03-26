b153ed49f73e357bf4428999e3d578a700e7ef06
AJ <joachim.liedtke@t-online.de>
2021-03-26 23:06:41 +0100

    feat(jwt): add jwt strategy and auth guard

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from 'src/user/user.module';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
+import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
-  imports: [UserModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({ secret: jwtConstants.secret, signOptions: { expiresIn: '1d' } }), // numbers are interpreted as seconds, strings are interpreted as ms, unless suffixed by a time unit identifier
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
