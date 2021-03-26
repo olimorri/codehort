f14df6120a6374f49d9032d95854b556c1c38b63
AJ <joachim.liedtke@t-online.de>
2021-03-26 18:29:25 +0100

    feat(auth.module.ts): add PassportModule to imports and LocalStrategy to exports

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from 'src/user/user.module';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [UserModule],
-  providers: [AuthService],
+  imports: [UserModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
