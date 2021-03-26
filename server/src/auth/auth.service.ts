993d41f1353b81992d6b936a8c8ee1ce068f3ad7
AJ <joachim.liedtke@t-online.de>
2021-03-26 18:54:34 +0100

    fix(auth.service.ts): restric returned user info to actually useful information

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/user/user.schema';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UserService) {}
 
-  async validateUser(username: string, pass: string): Promise<any> {
+  async validateUser(username: string, pass: string): Promise<User> {
     const user = await this.userService.getUserInfo(username);
     if (user && user.password === pass) {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { password, ...result } = user;
-      return result;
+      // const { password, ...result } = user;
+      // return result;
+      const thisUser = new User();
+      thisUser.id = user.id;
+      thisUser.username = user.username;
+      thisUser.password = user.password;
+      thisUser.email = user.email;
+
+      return thisUser;
     }
     return null;
   }
