86addfbd2fd2b3ce68b0c5946f7f4a787f90cab9
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-03-26 18:06:25 +0000

    feat(userrewards): connected userRewards to db and test - all works but need to associate to user

--- server/src/database/database.providers.ts
+++ server/src/database/database.providers.ts
@@ -5,6 +5,7 @@ import { Solution } from 'src/solution/solution.schema';
 import { Summary } from 'src/summary/summary.schema';
 import { Task } from 'src/task/task.schema';
 import { UserLesson } from 'src/user-lesson/userLesson.schema';
+import { UserReward } from 'src/user-rewards/userReward.schema';
 import { UserTest } from 'src/user-test/userTest.schema';
 import { User } from 'src/user/user.schema';
 import { databaseConfig } from './database.config';
@@ -32,8 +33,9 @@ export const databaseProviders = [
         Hint,
         Summary,
         UserTest,
+        UserReward,
       ]);
-      await sequelize.sync();
+      await sequelize.sync({ force: true });
       return sequelize;
     },
   },
