711d874f5437fe3f3a68d2ce0d59a3af3db3509e
Colin Bellamy <colin.j.bellamy@gmail.com>
2021-03-27 10:25:31 +0000

    fix(reward): fix rendering

--- client/src/components/Dashboard/RewardList/RewardList.tsx
+++ client/src/components/Dashboard/RewardList/RewardList.tsx
@@ -4,13 +4,12 @@ import { Reward } from '../../../components';
 
 export default function RewardList(props: IRewardListProps): JSX.Element {
   const userRewards: IUserReward[] = props.userRewards;
+  console.log(userRewards.length);
 
   return (
     <div className="reward-list">
       {userRewards.length ? (
-        userRewards.map((reward) => {
-          <Reward key={reward.lessonId} lessonId={+reward.lessonId} />;
-        })
+        userRewards.map((reward) => <Reward key={reward.lessonId} lessonId={reward.lessonId} />)
       ) : (
         <p className="notice">You don't have any rewards yet...</p>
       )}
