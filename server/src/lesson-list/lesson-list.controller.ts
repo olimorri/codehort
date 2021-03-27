6942ce0a7a5114cca34e274486fba873e6209ab9
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-03-27 11:01:13 +0000

    feat: update controllers

--- server/src/lesson-list/lesson-list.controller.ts
+++ server/src/lesson-list/lesson-list.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { LessonListDto } from './dto/lesson-list.dto';
+import { LessonListService } from './lesson-list.service';
 
 @Controller('lesson-list')
-export class LessonListController {}
+export class LessonListController {
+  constructor(private lessonListService: LessonListService) {}
+
+  @Post()
+  async addLesson(@Body() newLessonListItem: LessonListDto) {
+    return await this.lessonListService.addLessonToList({ ...newLessonListItem });
+  }
+
+  @Get()
+  async getList() {
+    return await this.lessonListService.getLessonList();
+  }
+}
