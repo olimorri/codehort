efd4d5580e07737450945c67858766701f95294d
AJ <joachim.liedtke@t-online.de>
2021-03-27 01:55:28 +0100

    fix(local.strategy.ts): improve return type

--- server/src/auth/local.strategy.ts
+++ server/src/auth/local.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
+import { User } from 'src/user/user.schema';
 import { AuthService } from './auth.service';
 
 @Injectable()
@@ -9,7 +10,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     super();
   }
 
-  async validate(username: string, password: string): Promise<any> {
+  async validate(username: string, password: string): Promise<User> {
     const user = await this.authService.validateUser(username, password);
     if (!user) throw new UnauthorizedException();
     return user;
