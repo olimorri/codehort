c20060aadf36aa577d886dbc5b602234415b166e
AJ <joachim.liedtke@t-online.de>
2021-03-27 16:58:49 +0100

    feat(app.controller, user.controller): finalize server side jwt handling

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Body, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './auth/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public() // TODO: remove for production
   @Post('create-lesson')
   async createLesson(@Body() newLesson): Promise<string> {
     return await this.appService.createLesson(newLesson);
--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Post, Get, Param, HttpCode, NotFoundException } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Param,
+  HttpCode,
+  NotFoundException,
+  UseGuards,
+} from '@nestjs/common';
+import { LocalAuthGuard } from 'src/auth/local-auth.guards';
+import { Public } from 'src/auth/public.decorator';
 import { UserDto } from './dto/user.dto';
 import { UserService } from './user.service';
 
@@ -6,6 +16,7 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private userService: UserService) {}
 
+  @Public()
   @Get('profile/:username')
   async getProfile(@Param('username') username: string): Promise<UserDto> {
     const profile = await this.userService.getUserInfo(username);
@@ -13,6 +24,8 @@ export class UserController {
     return profile;
   }
 
+  @Public() // make publicly accessible without jwt
+  @UseGuards(LocalAuthGuard) // but protect with username/password authentication
   @Post('logout')
   @HttpCode(200)
   logout(): string {
