0cca0d76c77f1f2c2e3f7e346aaddd2be9e0ae60
Colin Bellamy <colin.j.bellamy@gmail.com>
2021-03-28 18:10:06 +0100

    feat(progressbar): add custom progress bar

--- client/src/components/Dashboard/UserLesson/UserLesson.tsx
+++ client/src/components/Dashboard/UserLesson/UserLesson.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { IUserLessonProps } from '../../../interfaces';
+import ProgressBarFill from '../ProgressBarFill/ProgressBarFill';
 
 export default function UserLesson(props: IUserLessonProps): JSX.Element {
+  const progress = 10;
   return (
     <div className="user-lesson">
-      <Link to={`/lesson/${props.lessonId}`} className="link">
-        {props.name} <br />
-        <br />
-        Progress {(props.stepCompleted / 6) * 100}%
-      </Link>
+      <div className="title">
+        <Link to={`/lesson/${props.lessonId}`} className="link">
+          {props.name} <br />
+          <br />
+        </Link>
+      </div>
+      <div className="progress">{Array(progress).fill(<ProgressBarFill />)}</div>
     </div>
   );
 }
