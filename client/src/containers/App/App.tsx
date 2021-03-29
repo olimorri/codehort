bcdbe3a43cea1b47933d2b80eaeb1055480b60ad
AJ <joachim.liedtke@t-online.de>
2021-03-29 16:44:27 +0200

    refactor(loginform.tsx, app.tsx): minor code cleanup

--- client/src/containers/App/App.tsx
+++ client/src/containers/App/App.tsx
@@ -1,26 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { AppState, persistor } from '../../store/configureStore';
 import { Dashboard, Error, Landing, Lesson, LessonsOverview } from '../../containers';
 import { RouterGuard, NavBar, LoginForm, RegisterForm } from '../../components/';
-import { getUser } from '../../lib/apiService';
-import { setAuthenticated, setUser } from '../../actions';
 import { PersistGate } from 'redux-persist/integration/react';
 
 function App(): JSX.Element {
-  // const isAuthenticated = useSelector((state: AppState) => state.user.isAuthenticated);
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   if (!isAuthenticated) {
-  //     getUser().then((user) => {
-  //       dispatch(setUser(user));
-  //       dispatch(setAuthenticated(true));
-  //     });
-  //   }
-  // });
-
   const isLoggedIn = useSelector((state: AppState) => state.user.isAuthenticated);
   console.log('App.tsx isLoggedIn', isLoggedIn);
 
