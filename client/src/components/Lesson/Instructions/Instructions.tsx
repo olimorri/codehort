8705b457a6584c1b5a6aeeb9421ec14063f354cb
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-03-30 12:46:45 +0100

    style(instrutctions): removed unnecessary code and commented out items, component now good to go

--- client/src/components/Lesson/Instructions/Instructions.tsx
+++ client/src/components/Lesson/Instructions/Instructions.tsx
@@ -4,11 +4,7 @@ import { AppState } from '../../../store/configureStore';
 
 export default function Instructions(): JSX.Element {
   const tasks = useSelector((state: AppState) => state.lesson.lesson.task);
-  //const lesson = useSelector((state: AppState) => state.lesson.lesson);
-  const userLesson = useSelector((state: AppState) => state.userLessons.userLesson); //does this need to be a specific lesson
-
-  // const currentLesson =
-  //   userLessons.find((userLesson) => lesson.id === userLesson.lessonId)?.stepCompleted ?? 100; // 100 is to ensure that this is a number, and to easily debug if not a number. Low priority: this can undoubtedly be solved in a less ugly way.
+  const userLesson = useSelector((state: AppState) => state.userLessons.userLesson);
 
   return (
     <div className="instructions">{<p>{tasks?.[userLesson.stepCompleted]?.explanation}</p>}</div>
