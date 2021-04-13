3c5ec1f4545fded73fdda0b4714052aa65faa92b
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-04-13 10:24:07 +0100

    fix(userlesson): added a dynamic id, but needs to be updated from the db

--- client/src/components/Dashboard/UserLesson/UserLesson.tsx
+++ client/src/components/Dashboard/UserLesson/UserLesson.tsx
@@ -5,9 +5,13 @@ import ProgressBarFill from '../ProgressBarFill/ProgressBarFill';
 
 export default function UserLesson(props: IUserLessonProps): JSX.Element {
   const progress = Math.floor((props.stepCompleted / props.totalSteps) * 10);
+
+  //Placeholder in order to create dynamic id
+  const splitName = props.name.split(' ');
+  const htmlID = splitName[2];
   return (
     <div className="user-lesson">
-      <Link to={`/lesson/${props.lessonId}`} className="link">
+      <Link to={`/lesson/${props.lessonId}`} className="link" id={htmlID}>
         <div className="title">{props.name}</div>
         <div className="progress">{Array(progress).fill(<ProgressBarFill />)}</div>
       </Link>
